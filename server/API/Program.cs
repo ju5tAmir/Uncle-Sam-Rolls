dc89b1ebf02e953770bccbefb5c10d409f97e09a
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-09-24 13:44:34 +0200

    Added database connection

--- server/API/Program.cs
+++ server/API/Program.cs
@@ -1,6 +1,28 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UncleSamContext>(options =>
+{
+    options.UseNpgsql(
+        $"Host={Environment.GetEnvironmentVariable("DB_Host")};" +
+        $"Database={Environment.GetEnvironmentVariable("DB_Database")};" +
+        $"User Id={Environment.GetEnvironmentVariable("DB_Username")};" +
+        $"Password={Environment.GetEnvironmentVariable("DB_Password")};"
+        );
+});
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
+app.UseCors(options =>
+{
+    options.SetIsOriginAllowed(_ => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+});
 app.Run();
