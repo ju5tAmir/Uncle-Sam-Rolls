4918d96998a1c9dd8d36db15ad6ba9b469aeeda1
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-09-24 13:46:51 +0200

    Fixing bugs related to the references

--- server/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ server/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0642032915a784cf6aa0af4d1bf3ea7a72eae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ae623a4b1c98a5e5fee2511d2f5dd0ab623f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
