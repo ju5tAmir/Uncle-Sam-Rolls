b0ae623a4b1c98a5e5fee2511d2f5dd0ab623f73
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-09-24 13:24:31 +0200

    Adding missing references

--- server/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ server/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34af8f59e105b4c6bf624cafb162fd66d4ae9c5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0642032915a784cf6aa0af4d1bf3ea7a72eae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/DataAccess/UncleSamContext.cs
+++ server/DataAccess/UncleSamContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using DataAccess.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess;
 
